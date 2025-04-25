5640848cf0481280f3e4fd547b929bf807d0d7cf
ZM-7 <combiasso57@gmail.com>
2025-04-25 08:55:00 +0100

    Added event system + basic test

--- Juniper/src/Juniper/Application.cpp
+++ Juniper/src/Juniper/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Events/ApplicationEvent.h"
+#include "Log.h"
+
 namespace Juniper {
 
 	Application::Application()
@@ -13,6 +16,16 @@ namespace Juniper {
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			JP_TRACE("{}",e.ToString());
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			JP_TRACE("{}", e.ToString());
+		}
+
 		while (true);
 	}
 }
--- Juniper/src/Juniper/Log.h
+++ Juniper/src/Juniper/Log.h
@@ -4,6 +4,7 @@
 
 #include "Core.h"
 #include "spdlog/spdlog.h"
+#include "spdlog/fmt/ostr.h"
 
 namespace Juniper {
 	class JUNIPER_API Log
