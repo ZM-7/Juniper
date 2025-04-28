2a95156c3ba3655448e46a93df54fc662523833e
ZM-7 <combiasso57@gmail.com>
2025-04-28 18:04:24 +0100

    Setting up Mouse Events

--- Juniper/src/Platform/Windows/WindowsWindow.cpp
+++ Juniper/src/Platform/Windows/WindowsWindow.cpp
@@ -96,6 +96,42 @@ namespace Juniper {
 				}
 			});
 
+			glfwSetMouseButtonCallback(m_Window, [](GLFWwindow* window, int button, int action, int mods)
+			{
+					WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+					switch (action)
+					{
+						case GLFW_PRESS:
+						{
+							MouseButtonPressedEvent event(button);
+							data.EventCallback(event);
+							break;
+						}
+						case GLFW_RELEASE:
+						{
+							MouseButtonReleasedEvent event(button);
+							data.EventCallback(event);
+							break;
+						}
+					}
+			});
+
+			glfwSetScrollCallback(m_Window, [](GLFWwindow* window, double xOffset, double yOffset)
+				{
+					WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+					MouseScrolledEvent event((float)xOffset, (float)yOffset);
+					data.EventCallback(event);
+				});
+
+			glfwSetCursorPosCallback(m_Window, [](GLFWwindow* window, double xPos, double yPos)
+				{
+					WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+					MouseMovedEvent event((float)xPos, (float)yPos);
+					data.EventCallback(event);
+				});
 	}
 
 	void WindowsWindow::Shutdown()
